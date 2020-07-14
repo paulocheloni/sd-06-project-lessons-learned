8193d7afc0cedfa949a04a9de81c0d00cb780a3c
Artur Forna <fornagieri.artur@gmail.com>
2020-07-14 18:18:34 -0300

    Projeto Finalizado

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
 //Seleciona todas os elementos divs que possuem em sua classe o nome "lesson"
 $(document).ready(function(){
 
-
   $("#btn-about").click(function(e){
-  
-    $(".main-title").toggle();
-    $(".about-me-info").toggle();
-
+    if($(".section-1").is(":visible")){
+      $(".section-1").fadeOut("slow");
+      $(".about-me-info").fadeIn("slow");
+      $(".links-section").fadeIn("slow");
+    }else{
+      $(".about-me-info").fadeIn("slow");
+      $(".links-section").fadeIn("slow");
+    }
     e.preventDefault();
+  });
 
+  $(".lesson1").click(function(e){
+    if($(".about-me-info").is(":visible") ){
+      $(".about-me-info").fadeOut("slow");
+      $(".links-section").fadeOut("slow");
+      $(".section-1").fadeIn("slow");
+    }else{
+      $(".section-1").fadeIn("slow");
+    }
+    e.preventDefault();
   });
 
 });
