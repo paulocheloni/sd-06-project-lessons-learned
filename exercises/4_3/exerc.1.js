ded9d0af2e017fd6cd473a9cff8cedf5a6c282c0
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-07-15 15:51:48 -0300

    primeiro exerc. asterisco

---
+++ exercises/4_3/exerc.1.js
@@ -0,0 +1,14 @@
+let n = 5;
+
+
+for (let i = 0; i < n; i++) {
+    let linha = "";
+    for (let coluna = 0; coluna < n; coluna++) {
+
+        linha = linha + "*"
+    }
+    console.log(linha);
+
+
+
+}
